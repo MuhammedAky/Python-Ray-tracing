98fd2ad124d7a541fe7f3ee068a31fb63273c72d
MuhammedAky <muhammedaky24@gmail.com>
2022-10-26 22:17:49 +0300

    Lec 3

--- color.py
+++ color.py
@@ -1,4 +1,11 @@
 from vector import Vector
 
 class Color(Vector):
-    pass
+
+    @classmethod
+    def from_hex(cls, hecolor = "#000000"):
+        x = int(hecolor[1:3], 16) / 255.0
+        y = int(hecolor[3:5], 16) / 255.0
+        z = int(hecolor[5:7], 16) / 255.0
+
+        return cls(x,y,z)
