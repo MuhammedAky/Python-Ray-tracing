0493f1140ec948c625d337a08928b2a676f5e031
MuhammedAky <muhammedaky24@gmail.com>
2022-11-01 20:54:12 +0300

    Lec 4

--- engine.py
+++ engine.py
@@ -1,4 +1,3 @@
-from turtle import width
 from color import Color
 from image import Image
 from point import Point
@@ -24,9 +23,10 @@ class RenderEngine:
             y = y0 + j * ystep
             for i in range(width):
                 x = x0 + i * xstep
-                ray = Ray(camera, Point(x,y) - camera)
-                pixels.set_pixel(i, j, self.ray_trace(ray,scene))
-            return pixels
+                ray = Ray(camera, Point(x, y) - camera)
+                pixels.set_pixel(i, j, self.ray_trace(ray, scene))
+            print("{:3.0f}%".format(float(j) / float(height) * 100), end="\r")
+        return pixels
 
     def ray_trace(self, ray, scene):
         color = Color(0,0,0)
@@ -35,7 +35,8 @@ class RenderEngine:
             return color
 
         hit_pos = ray.origin + ray.direction * dist_hit
-        color += self.color_at(obj_hit, hit_pos, scene)
+        hit_normal = obj_hit.normal(hit_pos)
+        color += self.color_at(obj_hit, hit_pos, hit_normal, scene)
         return color
 
     def find_nearest(self,ray,scene):
@@ -48,5 +49,26 @@ class RenderEngine:
                 obj_hit = obj
             return (dist_min, obj_hit)
 
-    def color_at(self, obj_hit, hit_pos, scene):
-        return obj_hit.material
+    def color_at(self, obj_hit, hit_pos, normal, scene):
+        material = obj_hit.material
+        obj_color = material.color_at(hit_pos)
+        to_cam = scene.camera - hit_pos
+        specular_k = 50
+        color = material.ambient * Color.from_hex("#000000")
+        # Light calculations
+        for light in scene.lights:
+            to_light = Ray(hit_pos, light.position - hit_pos)
+            # Diffuse shading (Lambert)
+            color += (
+                obj_color
+                * material.diffuse
+                * max(normal.dot_product(to_light.direction), 0)
+            )
+            # Specular shading (Blinn-Phong)
+            half_vector = (to_light.direction + to_cam).normalize()
+            color += (
+                light.color
+                * material.specular
+                * max(normal.dot_product(half_vector), 0) ** specular_k
+            )
+        return color
--- scene.py
+++ scene.py
@@ -1,5 +1,5 @@
 class Scene:
-    def __init__(self, camera, objects, width, height):
+    def __init__(self, camera, objects, lights ,width, height):
         self.camera = camera
         self.objects = objects
         self.width = width
